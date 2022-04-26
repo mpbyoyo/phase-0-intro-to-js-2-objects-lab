cd4d658cbda72e10e94d5a650514862a044e1c5c
Maxwell Bourgeois <maxwellbourgeois@gmail.com>
2022-04-26 09:00:31 -0500

    commit tree e6f8a29f9843263b94d01083404050f0316008fc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+  name: "maxwell",
+  streetAddress: "283 Street"
+}
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  const result = {...employee}
+  result[key]=value
+  return result
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value
+
+  return employee;
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  const result = {...employee}
+  delete result[key]
+  return result
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key]
+  return employee
+}
